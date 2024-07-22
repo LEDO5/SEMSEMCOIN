0946cfb3502e10c8fc2734911cd1e1afcb6679b9
LEDO5 <xmax11prombox@gmail.com>
2024-07-23 02:43:35 +0300

    Delete vite.config.mjs

--- vite.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  plugins: [],
-  server: {
-    host: '0.0.0.0',
-    port: 5173, // you can change the port if needed
-  },
-});
